444f54e74311b27bee5e964092f3d99191de380c
drScripts <nathanael.vd@gmail.com>
2022-05-31 14:49:41 +0700

    update

--- index.js
+++ index.js
@@ -8,20 +8,19 @@ function canSellSocks(array) {
   });
 
   for (let key in counts) {
-    if (!counts.hasOwnProperty(key)) continue;
-
     let obj = counts[key];
     if (obj % 2 != obj) {
-      length++;
+      const value = Math.floor(obj / 2);
+      length += value;
     }
   }
 
   return length;
 }
 
-function getLetterLength(string) {
+function getWordLength(string) {
   let counter = 0;
-  const format = /[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+  const format = /[ `!@#$%^&*()_+\=\[\]{};':"\\|<>\/~]/;
 
   string.split(" ").forEach((data) => {
     if (!format.test(data)) {
@@ -33,5 +32,14 @@ function getLetterLength(string) {
 }
 
 console.log(canSellSocks(arrayKaoskaki));
+console.log(canSellSocks([10, 20, 20, 10, 10, 30, 50, 10, 20]));
+console.log(canSellSocks([6, 5, 2, 3, 5, 2, 2, 1, 1, 5, 1, 3, 3, 3, 5]));
+console.log(canSellSocks([1, 1, 3, 1, 2, 1, 3, 3, 3, 3]));
 
-console.log(getLetterLength("Kemarin Shopia per[gi ke mall."));
+console.log("WORD");
+console.log(getWordLength("Kemarin Shopia per[gi ke mall."));
+console.log(getWordLength("Saat meng*ecat tembok, Agung dib_antu oleh Raihan"));
+console.log(getWordLength("Berapa u(mur minimal[ untuk !mengurus ktp?"));
+console.log(
+  getWordLength("Masing-masing anak mendap(atkan uang jajan ya=ng be&rbeda.")
+);
